dca4377554c49314ffd9f78dfe4d20742d73265e
Rohit Shinde <rohitshindee0501@gmail.com>
2025-02-04 15:24:52 +0530

    code change

--- src/Landing_page/about/AboutPage.js
+++ src/Landing_page/about/AboutPage.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Hero from './Hero';
+import Team from './Team';
 
 function AboutPage() {
     return (
         <div>
             <h1>About Page</h1>
+            <>
+            <Hero/>
+            <Team/>
+            </>
         </div>
       );
 }
